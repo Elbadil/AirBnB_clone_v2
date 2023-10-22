85154ad8c690bd8340e89c69a463c2ea107a57a4
Elbadil <elbaliadil@gmail.com>
2023-10-22 21:34:35 +0100

    HBNB filters

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 from models import storage
 
 
@@ -13,15 +14,16 @@ app.static_folder = 'static'
 app.static_url_path = '/static'
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     """
     display a HTML page like Airbnb states and amenities
     on 0.0.0.0 port 5000
     """
     states = storage.all(State)
     amenities = storage.all(Amenity)
-    return render_template('10-hbnb_filters.html', state=states, am=amenities)
+    places = storage.all(Place)
+    return render_template('100-hbnb.html', state=states, am=amenities, place=places)
 
 
 @app.teardown_appcontext
